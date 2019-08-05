85eab06aa1c31ddbd14a7a09ac4e74c87aefba52
Briana Presley <presley.briana@gmail.com>
2019-08-05 16:35:46 -0400

    Added SQL variables

--- CSharp2SQLProject/Program.cs
+++ CSharp2SQLProject/Program.cs
@@ -6,6 +6,23 @@ using System.Data.SqlClient;
 namespace CSharp2SQLProject {
     class Program {
         static void Main(string[] args) {
+            var ord = GetOrderById(2007);
+            if (ord == null) {
+                Console.WriteLine("Order not found");
+            } else {
+                Console.WriteLine($"{ord.ID} | {ord.Date}");
+            }
+
+
+            var cust = GetCustomerById(1028);
+            if (cust == null) {
+                Console.WriteLine("Customer not found");
+            }
+            else {
+                Console.WriteLine($"{cust.Name}");
+            }
+
+
             var sql = "SELECT * from Orders;";
             var orders = SelectOrder(sql); //Can also input a valid SQL statement in (sql)
             foreach (var order in orders) {
@@ -27,63 +44,122 @@ namespace CSharp2SQLProject {
             var connect = new SqlConnection(connOrders);
             connect.Open();
             if (connect.State != ConnectionState.Open) {
-                throw new Exception("Connection did not open!"); 
-                }
-                var orderList = new List<Order>();
-                var ordercmd = new SqlCommand(sql, connect);
-                var reader = ordercmd.ExecuteReader();
+                throw new Exception("Connection did not open!");
+            }
+            var orderList = new List<Order>();
+            var ordercmd = new SqlCommand(sql, connect);
+            var reader = ordercmd.ExecuteReader();
 
-                while (reader.Read()) {
-                    var id = (int)reader["Id"];
-                    var date = (DateTime)reader["Date"];
-                    var notes = reader.IsDBNull(reader.GetOrdinal("Note"))
-                        ? null
-                        : reader["Note"].ToString();
-                    var CustomerId = (int)reader["CustomerId"];
+            while (reader.Read()) {
+                var id = (int)reader["Id"];
+                var date = (DateTime)reader["Date"];
+                var notes = reader.IsDBNull(reader.GetOrdinal("Note"))
+                    ? null
+                    : reader["Note"].ToString();
+                var CustomerId = (int)reader["CustomerId"];
                 var order = new Order(id, date, notes, CustomerId);
                 orderList.Add(order);
-                }
+            }
             reader.Close();
 
             connect.Close();
             return orderList;
-            }   
+        }
 
         static List<Customer> SelectCustomer(string sql) { //To access SQL from C# 
-                //Step 1!!
-                var connStr = @"server=localhost\sqlexpress;database=CustomerOrderDb;trusted_connection=true;"; // To identify the server, the instance; database name and authentication;
-                //Step 2!! Create an instance
-                var connection = new SqlConnection(connStr);
-                //Step 3!! Check to see if connection opened
-                connection.Open();
-                if (connection.State != ConnectionState.Open) { //To determine if connection is open != means is not
-                    throw new Exception("Connection did not open!"); //To create an error if connection not opened successfully
-                }
-                var customerList = new List<Customer>();
-                //var sql = "SELECT * from Customers Where State = 'OH';"; //Enter SQL Statement
-                //Step 4!! Create a SQL command
-                var cmd = new SqlCommand(sql, connection);
-                //Step 5!! Create a SQL reader and give it a variable name
-                var reader = cmd.ExecuteReader();
-                //Read statement moves the pointer down to the next row of sql table; returns a boolean; usually false when there are no more rows
-                //Step 6!! Create a while loop to read all data in a selected table/column
-                while (reader.Read()) { // Access each column by identifying the column name but you must change/cast the object to the correct data type
-                    var id = (int)reader["ID"]; //Input an index into square brackets, 0 is column 1; or just input the column name
-                    var name = reader["Name"].ToString();
-                    var city = reader["City"].ToString();
-                    var state = reader["State"].ToString();
-                    var active = (bool)reader["Active"];
-                    var code = reader.IsDBNull(reader.GetOrdinal("Code")) //If you have to check for Null, use 'IsDBNull'
-                        ? null //If Column "Code" has a field that is null, display null; else display value in the field as a string
-                        : reader["Code"].ToString();
-                    var customer = new Customer(id, name, city, state, active, code);
-                    customerList.Add(customer); //Adds custmer to the list
-                }
-
-                reader.Close(); //Good practice to close the data reader after program runs
-                connection.Close(); //To close connection after program runs
-                return customerList;
+                                                           //Step 1!!
+            var connStr = @"server=localhost\sqlexpress;database=CustomerOrderDb;trusted_connection=true;"; // To identify the server, the instance; database name and authentication;
+                                                                                                            //Step 2!! Create an instance
+            var connection = new SqlConnection(connStr);
+            //Step 3!! Check to see if connection opened
+            connection.Open();
+            if (connection.State != ConnectionState.Open) { //To determine if connection is open != means is not
+                throw new Exception("Connection did not open!"); //To create an error if connection not opened successfully
+            }
+            var customerList = new List<Customer>();
+            //var sql = "SELECT * from Customers Where State = 'OH';"; //Enter SQL Statement
+            //Step 4!! Create a SQL command
+            var cmd = new SqlCommand(sql, connection);
+            //Step 5!! Create a SQL reader and give it a variable name
+            var reader = cmd.ExecuteReader();
+            //Read statement moves the pointer down to the next row of sql table; returns a boolean; usually false when there are no more rows
+            //Step 6!! Create a while loop to read all data in a selected table/column
+            while (reader.Read()) { // Access each column by identifying the column name but you must change/cast the object to the correct data type
+                var id = (int)reader["ID"]; //Input an index into square brackets, 0 is column 1; or just input the column name
+                var name = reader["Name"].ToString();
+                var city = reader["City"].ToString();
+                var state = reader["State"].ToString();
+                var active = (bool)reader["Active"];
+                var code = reader.IsDBNull(reader.GetOrdinal("Code")) //If you have to check for Null, use 'IsDBNull'
+                    ? null //If Column "Code" has a field that is null, display null; else display value in the field as a string
+                    : reader["Code"].ToString();
+                var customer = new Customer(id, name, city, state, active, code);
+                customerList.Add(customer); //Adds custmer to the list
             }
 
+            reader.Close(); //Good practice to close the data reader after program runs
+            connection.Close(); //To close connection after program runs
+            return customerList;
         }
+        static Order GetOrderById(int oId) {
+            var connStr = @"server=localhost\sqlexpress;database=CustomerOrderDb;trusted_connection=true;";
+            var connection = new SqlConnection(connStr);
+            connection.Open();
+            if (connection.State != ConnectionState.Open) {
+                throw new Exception("Connection did not open!");
+            }
+            var sql = "SELECT * From Orders Where Id = @orderid;";
+            var cmd = new SqlCommand(sql, connection);
+            var theId = new SqlParameter("@orderid", oId);
+            cmd.Parameters.Add(theId);
+            var reader = cmd.ExecuteReader();
+            Order ord = null;
+            if (reader.Read()) {
+                var id = (int)reader["Id"];
+                var date = (DateTime)reader["Date"];
+                var notes = reader.IsDBNull(reader.GetOrdinal("Note"))
+                    ? null
+                    : reader["Note"].ToString();
+                var CustomerId = (int)reader["CustomerId"];
+                ord = new Order(id, date, notes, CustomerId);
+                
+            }
+            reader.Close();
+            connection.Close();
+            return ord;
+        }
+        
+
+
+        static Customer GetCustomerById(int pId) {
+            var connStr = @"server=localhost\sqlexpress;database=CustomerOrderDb;trusted_connection=true;";
+            var connection = new SqlConnection(connStr);
+            connection.Open();
+            if (connection.State != ConnectionState.Open) {
+                throw new Exception("Connection did not open!");
+            }
+            var sql = "SELECT * From Customers Where Id = @myid;";
+            var cmd = new SqlCommand(sql, connection);
+            var theId = new SqlParameter("@myid", pId);
+            cmd.Parameters.Add(theId);
+            var reader = cmd.ExecuteReader();
+            Customer cust = null;
+            if (reader.Read()) {
+                var id = (int)reader["ID"]; //Input an index into square brackets, 0 is column 1; or just input the column name
+                var name = reader["Name"].ToString();
+                var city = reader["City"].ToString();
+                var state = reader["State"].ToString();
+                var active = (bool)reader["Active"];
+                var code = reader.IsDBNull(reader.GetOrdinal("Code")) //If you have to check for Null, use 'IsDBNull'
+                    ? null //If Column "Code" has a field that is null, display null; else display value in the field as a string
+                    : reader["Code"].ToString();
+                cust = new Customer(id, name, city, state, active, code);
+
+            }
+            reader.Close();
+            connection.Close();
+            return cust;
+        }
+
     }
+}
